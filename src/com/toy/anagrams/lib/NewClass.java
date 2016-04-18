2ab7646fa530f1f6390102fb639aadf2f969fa9e
J. Kotorowicz <>
2016-04-18 12:50:46 +0200

    jeszcze jedna klasa

---
+++ src/com/toy/anagrams/lib/NewClass.java
@@ -0,0 +1,13 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.toy.anagrams.lib;
+
+/**
+ *
+ * @author student
+ */
+public class NewClass {
+    
+}
